d8bebd6eab11ecd7f9ee5e74520648ae5129a6f3
Chris <chris.alafaa@gmail.com>
2022-02-18 12:40:50 +0100

    I made some adjustments

--- index.js
+++ index.js
@@ -2,20 +2,10 @@ const express = require('express');
 const res = require('express/lib/response');
 const app = express();
 const port = 4000;
-// const requestTimeOut = require('./views/timeOut');
+const requestTimeOut = require('./middleware/timeOut');
 
 // use requestTimeOut middleware
-
-module.exports =(req, res, next) => {
-    let day = new Date().getDay();
-    let time = new Date().getHours;
-
-    if( time > 9 && time < 17 && day !== 0 && day !== 0) next()
-    else {
-        res.render("Site Unavailable")
-    }
-}
-// app.use(requestTimeOut);
+app.use(requestTimeOut);
 
 // set view engine
 app.set('view engine', 'ejs');
---
+++ middleware/timeOut.js
@@ -0,0 +1,11 @@
+
+module.exports =(req, res, next) => {
+    let day = new Date().getDay();
+    let time = new Date().getHours();
+
+    
+    if( time > 9 && time < 17 && day !== 0 && day !== 6) next() 
+    else {
+        res.render("pages/unavailable")
+    }
+}
